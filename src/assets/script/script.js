499d6885fb33f8ae37f7c136b4b2fe7f4048c636
Doneal Piqué <135713732+DonealPique@users.noreply.github.com>
2024-07-08 16:03:51 +0200

    wip

---
+++ src/assets/script/script.js
@@ -0,0 +1,8 @@
+// swiperjs pagination
+const swiper = new Swiper('.swiper', {
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+});
+// swiperjs pagination end
